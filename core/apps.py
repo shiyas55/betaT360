4dd5a89940a74c9cd7e0ca352fee0ed5355a08e3
shiyas ahmad <shiyasahmad555@gmail.com>
2026-06-30 16:54:42 +0530

    Fix signal disconnect syntax by using dispatch_uid='update_last_login'

--- core/apps.py
+++ core/apps.py
@@ -6,6 +6,5 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         # Disconnect last_login update signal to prevent database writes on user logins (crucial for read-only Vercel SQLite deploy)
-        from django.contrib.auth.models import update_last_login
         from django.contrib.auth.signals import user_logged_in
-        user_logged_in.disconnect(update_last_login)
+        user_logged_in.disconnect(dispatch_uid='update_last_login')
