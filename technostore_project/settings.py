15f46f3a98e891c592e7c379beab9abf3f0e99dd
shiyas ahmad <shiyasahmad555@gmail.com>
2026-06-30 16:58:56 +0530

    Clean up temporary debug traceback middleware

--- technostore_project/settings.py
+++ technostore_project/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'technostore_project.middleware.ExceptionDisplayMiddleware',
 ]
 
 ROOT_URLCONF = 'technostore_project.urls'
