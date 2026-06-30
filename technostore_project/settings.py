2a5e79acf321cf8e0aa62a31c625842ce61ed828
shiyas ahmad <shiyasahmad555@gmail.com>
2026-06-30 16:20:02 +0530

    Configure nested static collection directory in settings.py and update .gitignore

--- technostore_project/settings.py
+++ technostore_project/settings.py
@@ -107,7 +107,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
