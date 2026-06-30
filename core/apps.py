aa305b4d5eb5684e5e963711eed45843b0f2256a
shiyas ahmad <shiyasahmad555@gmail.com>
2026-06-30 16:42:06 +0530

    Disconnect user_logged_in update_last_login receiver to allow login on Vercel read-only sqlite database

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        # Disconnect last_login update signal to prevent database writes on user logins (crucial for read-only Vercel SQLite deploy)
+        from django.contrib.auth.models import update_last_login
+        from django.contrib.auth.signals import user_logged_in
+        user_logged_in.disconnect(update_last_login)
